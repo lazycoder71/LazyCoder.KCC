4c702896114c0d29226a7a1c2193bce6d09532ae
lamnd <lamnd.bounce@gmail.com>
2025-02-27 11:55:38 +0700

    + package.json

--- Runtime/KCCMotor.cs
+++ Runtime/KCCMotor.cs
@@ -189,7 +189,7 @@ namespace LFramework.KCC
         [SerializeField]
         [Tooltip("Physics material of the Character Capsule (Does not affect character movement. Only affects things colliding with it)")]
 #pragma warning disable 0649
-        private PhysicMaterial CapsulePhysicsMaterial;
+        private PhysicsMaterial CapsulePhysicsMaterial;
 #pragma warning restore 0649
 
 
@@ -1857,7 +1857,7 @@ namespace LFramework.KCC
                         bool hitBodyIsDynamic = !bodyHit.Rigidbody.isKinematic;
                         float hitBodyMass = bodyHit.Rigidbody.mass;
                         float hitBodyMassAtPoint = bodyHit.Rigidbody.mass; // todo
-                        Vector3 hitBodyVelocity = bodyHit.Rigidbody.velocity;
+                        Vector3 hitBodyVelocity = bodyHit.Rigidbody.linearVelocity;
                         if (hitBodyIsCharacter)
                         {
                             hitBodyMass = hitCharacterMotor.SimulatedCharacterMass;
@@ -2290,7 +2290,7 @@ namespace LFramework.KCC
         {
             if (deltaTime > 0f)
             {
-                linearVelocity = interactiveRigidbody.velocity;
+                linearVelocity = interactiveRigidbody.linearVelocity;
                 angularVelocity = interactiveRigidbody.angularVelocity;
                 if(interactiveRigidbody.isKinematic)
                 {
